3ac10a5ebbdee6d0b6b919ca1aa8ccd0595cf0ac
Will Campbell <wibb1@comcast.net>
2021-08-10 10:21:13 -0400

    Singly Linked List - push

---
+++ Section-19-SinglyLinkedLists/01-Push.js
@@ -0,0 +1,53 @@
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  myPush(val) {
+    let node = new Node(val);
+    if (this.head === null) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  hisPush(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+
+var list = new SinglyLinkedList()
+list.myPush("HI")
+list.myPush("YOU")
+list.myPush(99)
+console.log(list)
+
+var list1 = new SinglyLinkedList()
+list1.hisPush("HI")
+list1.hisPush("YOU")
+list1.hisPush(99)
+console.log(list1)
+
