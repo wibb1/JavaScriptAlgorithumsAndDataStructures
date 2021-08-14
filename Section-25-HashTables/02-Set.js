417456b916209adb5a3d6af8111bb1449655c739
Will Campbell <wibb1@comcast.net>
2021-08-14 01:57:37 -0400

    Hash Table Set

---
+++ Section-25-HashTables/02-Set.js
@@ -0,0 +1,22 @@
+class HashTable {
+  constructor(size = 53) {
+    this.keyMap = new Array(size);
+  }
+
+  _hash(key) {
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for (let i = 0; i < Math.min(key.length, 100); i++) {
+      let char = key[i];
+      let value = char.charCodeAt(value[i] - 96);
+      total = (total * WEIRD_PRIME + value) % this.keyMap.length;
+    }
+    return total;
+  }
+
+  set(key, value) {
+    let index = this._hash(key)
+    if(!this.keymap[index]) this.keyMap[index] = []
+    this.keyMap[index].push([key,value])
+  }
+}
